56d022312d1cd4223866eb7517b3058c338756dd
Alisa <alisa.luoma@hotmail.fi>
2023-03-15 13:32:31 +0200

    Added Redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Layout/Header";
+import { store } from "./Store/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Header/>
+    <Provider store={store}>
+      <Header />
+    </Provider>
   </React.StrictMode>
 );
